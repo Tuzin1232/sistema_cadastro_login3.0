b1ba8f398075a7b64f35294932042e6e21996d6c
Arthur Araújo <arthuraraujo07@gmail.com>
2021-10-19 11:50:32 -0300

    Update sistema_cadastro_login.sql

--- Projeto Exemplo 2.0/BD/sistema_cadastro_login.sql
+++ Projeto Exemplo 2.0/BD/sistema_cadastro_login.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Tempo de geração: 07-Out-2021 às 14:52
+-- Tempo de geração: 19-Out-2021 às 14:50
 -- Versão do servidor: 8.0.21
 -- versão do PHP: 7.3.21
 
@@ -30,31 +30,39 @@ SET time_zone = "+00:00";
 DROP TABLE IF EXISTS `clientes`;
 CREATE TABLE IF NOT EXISTS `clientes` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `avaliacao` tinyint(1) NOT NULL,
-  `tipo` varchar(100) NOT NULL,
-  `pend_financeira` tinyint(1) NOT NULL,
-  `mes_atraso` int NOT NULL,
-  `contrato_ativo` tinyint(1) NOT NULL,
-  `nome` varchar(150) NOT NULL,
-  `cnpj` varchar(14) NOT NULL,
-  `cpf` varchar(11) NOT NULL,
-  `telefone` varchar(10) NOT NULL,
-  `celular` varchar(11) NOT NULL,
-  `contato` varchar(100) NOT NULL,
-  `ramo_atividade` varchar(150) NOT NULL,
-  `cidade` varchar(255) NOT NULL,
-  `rua` varchar(255) NOT NULL,
-  `bairro` varchar(255) NOT NULL,
-  `numero` int NOT NULL,
-  `UF` varchar(2) NOT NULL,
-  `cep` int NOT NULL,
-  `complemento` varchar(50) NOT NULL,
-  `ip_acesso` varchar(255) NOT NULL,
-  `obv` varchar(255) NOT NULL,
-  `ativo` tinyint(1) NOT NULL,
-  `codigo_cliente` int NOT NULL,
+  `avaliacao` tinyint(1) DEFAULT NULL,
+  `tipo` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `pend_financeira` tinyint(1) DEFAULT NULL,
+  `mes_atraso` int DEFAULT NULL,
+  `contrato_ativo` tinyint(1) DEFAULT NULL,
+  `nome` varchar(150) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `cnpj` varchar(14) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `cpf` varchar(11) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `telefone` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `celular` varchar(11) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `contato` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `ramo_atividade` varchar(150) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `cidade` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `rua` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `bairro` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `numero` int DEFAULT NULL,
+  `UF` varchar(2) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `cep` int DEFAULT NULL,
+  `complemento` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `ip_acesso` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `obv` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `ativo` tinyint(1) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=31 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=36 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Extraindo dados da tabela `clientes`
+--
+
+INSERT INTO `clientes` (`id`, `avaliacao`, `tipo`, `pend_financeira`, `mes_atraso`, `contrato_ativo`, `nome`, `cnpj`, `cpf`, `telefone`, `celular`, `contato`, `ramo_atividade`, `cidade`, `rua`, `bairro`, `numero`, `UF`, `cep`, `complemento`, `ip_acesso`, `obv`, `ativo`) VALUES
+(34, 0, '', 0, 0, 0, 'aa', '', '', '', '', '', '', '', '', '', 0, '', 0, '', '', '', 0),
+(33, 0, '', 0, 0, 0, '', '', '', '', '', '', '', '', '', '', 0, '', 0, '', '', '', 0),
+(35, 0, '', 0, 0, 0, 'Caa', '', '', '', '', '', '', '', '', '', 0, '', 0, '', '', '', 0);
 
 -- --------------------------------------------------------
 
@@ -65,7 +73,6 @@ CREATE TABLE IF NOT EXISTS `clientes` (
 DROP TABLE IF EXISTS `clientesistemas`;
 CREATE TABLE IF NOT EXISTS `clientesistemas` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `cliente_id` int NOT NULL,
   `sistema` varchar(255) NOT NULL,
   `ativo` tinyint(1) NOT NULL,
   `data_inicio` date NOT NULL,
@@ -116,7 +123,14 @@ CREATE TABLE IF NOT EXISTS `usuario` (
   `nome` varchar(50) NOT NULL,
   `senha` varchar(50) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Extraindo dados da tabela `usuario`
+--
+
+INSERT INTO `usuario` (`id`, `nome`, `senha`) VALUES
+(9, 'teste', '123');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
